c401c7b620afd47115e6ee4425c53267c6189c66
Jared <ricjm008@mymail.unisa.edu.au>
2021-05-26 13:26:26 +0930

    add inquirer

--- server.js
+++ server.js
@@ -1,10 +1,76 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'mysqlpassword',
-    database: 'employees_db',
-  });
-  
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'mysqlpassword',
+        database: 'employees_db',
+    });
+connection.connect((err) => {
+    if (err) throw err;
+    runAction();
+});
+    
+const runAction = () => {
+inquirer
+    .prompt({
+    name: 'action',
+    type: 'rawlist',
+    message: 'What would you like to do?',
+    choices: [
+        'Add employee',
+        'Add role',
+        'Add department',
+        'View employees by role',
+        'View employees by department',
+        'View employees by manager',
+        'Update employee',
+        'Update role',
+        'Update department'
+    ],
+    })
+    .then((answer) => {
+    switch (answer.action) {
+        case 'Add employee':
+        addEmployee();
+        break;
+
+        case 'Add role':
+        addRole();
+        break;
+
+        case 'Add department':
+        addDepartment();
+        break;
+
+        case 'View employees by role':
+        tableEmployees(role);
+        break;
+
+        case 'View employees by department':
+        tableEmployees(department);
+        break;
+
+        case 'View employees by manager':
+        tableEmployees(manager);
+        break;
+
+        case 'Update employee':
+        updateEmployee();
+        break;
+
+        case 'Update role':
+        updateRole();
+        break;
+
+        case 'Update department':
+        updateDepartment();
+        break;
+
+        default:
+        console.log(`Invalid action: ${answer.action}`);
+        break;
+    }
+    });
+};
