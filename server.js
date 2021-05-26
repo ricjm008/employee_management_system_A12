206f18ca594ba01b8861be8270fccc8e384a2cb5
Jared <ricjm008@mymail.unisa.edu.au>
2021-05-26 10:25:40 +0930

    created schema template

---
+++ server.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'mysqlpassword',
+    database: 'employee_db',
+  });
+  
