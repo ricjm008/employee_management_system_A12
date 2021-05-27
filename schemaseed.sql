a3c3e5be75859132cbcb6299533c87a3da7b93f8
Jared <ricjm008@mymail.unisa.edu.au>
2021-05-27 21:40:11 +0930

    fixed schema

--- schemaseed.sql
+++ schemaseed.sql
@@ -1,14 +1,9 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
-CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+CREATE TABLE departments (
+    id INT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
@@ -18,8 +13,13 @@ CREATE TABLE roles (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES departments(id)
 );
-CREATE TABLE departments (
-    id INT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
